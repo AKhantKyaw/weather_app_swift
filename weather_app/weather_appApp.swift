5ba853d7d4fa59c216dcd06a52ea6c91c7156d28
AungKhantKyaw <103022859+AKhantKyaw@users.noreply.github.com>
2023-07-09 15:45:20 +0630

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Aung Khant Kyaw on 09/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
